3ea85210fa208440a4be497cfe7ae7e43c387772
Stephanie Hicks <stephaniechicks@gmail.com>
2019-07-09 21:02:04 -0400

    lowering mem_free parameter

--- patients/sc-dynamics-CML-analysis-2019-0114Patient-RNAseq-cluster.sh
+++ patients/sc-dynamics-CML-analysis-2019-0114Patient-RNAseq-cluster.sh
@@ -1,4 +1,4 @@
-#$ -l h_fsize=50G,mem_free=30G,h_vmem=30G
+#$ -l h_fsize=20G,mem_free=20G,h_vmem=20G
 #$ -cwd
 
 # Rscript /users/shicks1/projects/sc-dynamics-CML/patients/sc-dynamics-CML-analysis-2019-0114Patient-RNAseq-cluster.R

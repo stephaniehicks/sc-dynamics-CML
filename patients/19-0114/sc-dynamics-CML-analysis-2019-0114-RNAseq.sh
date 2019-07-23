268a8d9926eed643ae1b014427505834df30ebfc
Stephanie Hicks <stephaniechicks@gmail.com>
2019-07-22 23:27:50 -0400

    starting Rmd and bash scripts for all six patients

---
+++ patients/19-0114/sc-dynamics-CML-analysis-2019-0114-RNAseq.sh
@@ -0,0 +1,4 @@
+#$ -l mem_free=10G,h_vmem=10G
+#$ -cwd
+
+R -e "rmarkdown::render('/users/shicks1/projects/sc-dynamics-CML/patients/19-0114/sc-dynamics-CML-analysis-2019-0114-RNAseq.Rmd')"
--- patients/sc-dynamics-CML-analysis-2019-0114Patient-RNAseq-cluster.sh
+++
@@ -1,5 +0,0 @@
-#$ -l mem_free=10G,h_vmem=10G
-#$ -cwd
-
-# Rscript /users/shicks1/projects/sc-dynamics-CML/patients/sc-dynamics-CML-analysis-2019-0114Patient-RNAseq-cluster.R
-R -e "rmarkdown::render('/users/shicks1/projects/sc-dynamics-CML/patients/sc-dynamics-CML-analysis-2019-0114Patient-RNAseq.Rmd')"
